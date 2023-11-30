a09a48091c87843071e639446aeae0e0b55b3d9e
Sara <sara.667@outlook.com>
2023-11-29 19:36:10 -0700

    .

--- mean-project/src/app/posts/posts.service.ts
+++ mean-project/src/app/posts/posts.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Subject } from 'rxjs';
+import { Subject, map } from 'rxjs';
 
 import { Post } from './post.model';
 
@@ -13,11 +13,22 @@ export class PostsService {
 
   getPosts() {
     this.http
-      .get<{ message: string; posts: Post[] }>(
-        'http://localhost:3000/api/posts'
+      .get<{ message: string; posts: any }>('http://localhost:3000/api/posts')
+      .pipe(
+        map((postData) => {
+          return postData.posts.map(
+            (post: { title: any; content: any; _id: any }) => {
+              return {
+                title: post.title,
+                content: post.content,
+                id: post._id,
+              };
+            }
+          );
+        })
       )
-      .subscribe((postData) => {
-        this.posts = postData.posts;
+      .subscribe((transformedPosts) => {
+        this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
@@ -30,5 +41,15 @@ export class PostsService {
     const post: Post = { id: '', title: title, content: content };
     this.posts.push(post);
     this.postsUpdated.next([...this.posts]);
+  }
+
+  deletePost(postId: string) {
+    this.http
+      .delete('http://localhost:3000/api/posts/' + postId)
+      .subscribe(() => {
+        const updatedPosts = this.posts.filter((post) => post.id !== postId);
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 }
